5eb05cd3163047c6bcd7b3bd9c3bf12328d4de5f
moneer alromimah <moneer@moneers-MacBook-Pro.local>
2023-01-13 23:39:52 -0700

    Added eqArrays Function

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+// Function implmentaion
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`)
+  } else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`)
+}
+
+const eqArrays = (Arr1, Arr2) => {
+  if (Arr1.length !== Arr2.length) return false
+  else {
+    for (let i = 0; i < Arr1.length; i++) {
+      if (Arr1.length !== Arr2.length || Arr1[i] !== Arr2[i]) {
+        return false
+      }
+    }
+    return true
+  }
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
