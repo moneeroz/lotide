08da217533b0548fb4111a4056dcf5ca09f27264
moneer alromimah <moneer@moneers-MacBook-Pro.local>
2023-01-14 00:01:27 -0700

    Cleaned up the eqArrays Function

--- eqArrays.js
+++ eqArrays.js
@@ -6,16 +6,14 @@ const assertEqual = function (actual, expected) {
 }
 
 const eqArrays = (Arr1, Arr2) => {
-  if (Arr1.length !== Arr2.length) return false
-  else {
-    for (let i = 0; i < Arr1.length; i++) {
-      if (Arr1.length !== Arr2.length || Arr1[i] !== Arr2[i]) {
-        return false
-      }
+  for (let i = 0; i < Arr1.length; i++) {
+    if (Arr1.length !== Arr2.length || Arr1[i] !== Arr2[i]) {
+      return false
     }
-    return true
   }
+  return true
 }
+
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
