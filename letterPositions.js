4e7ba60054ce156d4243ff29b6b87790f908c25d
moneer alromimah <moneer@moneers-MacBook-Pro.local>
2023-01-15 23:09:14 -0700

    Added letterPositions Functions

---
+++ letterPositions.js
@@ -0,0 +1,11 @@
+const letterPositions = (str) => {
+  const results = {}
+  // code goes here
+  for (let i = 0; i < str.length; i++) {
+    if (i === ' ') {
+      continue
+    }
+    results[str[i]] ? results[str[i]].push(i) : results[str[i]] = [i]
+  }
+  console.log(results)
+}
